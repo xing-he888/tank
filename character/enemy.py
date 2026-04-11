6e59fee737e12cd29c4ba4f42a0ab0eaff8c1584
xing-he888 <2835584575@qq.com>
2026-04-11 19:08:18 +0800

    sss

--- character/enemy.py
+++ character/enemy.py
@@ -2,21 +2,24 @@ import pygame
 import time
 import random
 from character import player
+
+
 class Enemy:
-    # 此行size指的是半径
-    def __init__(self, image_file, size,blood):
+    def __init__(self, image_file, size, blood):
         self.original_image = pygame.image.load(image_file)
         self.size = size
-        self.x=random.randint(self.size,800-self.size)
-        self.y=random.randint(self.size,600-self.size)
-        self.image = pygame.transform.scale(self.original_image, (size*2,size*2))
+        self.x = random.randint(self.size, 800 - self.size)
+        self.y = random.randint(self.size, 600 - self.size)
+        self.original_image = pygame.transform.scale(self.original_image, (size * 2, size * 2))
+        self.image = self.original_image.copy()
         self.speed = 1
-        self.blood=blood
+        self.blood = blood
         self.last_attack_time = 0
-        self.attack_interval = 1000  # 攻击间隔（毫秒）
+        self.attack_interval = 1000
 
-    def draw(self,screen):  #hu绘制
-        screen.blit(self.image,(self.x-self.size,self.y-self.size))
+    def draw(self, screen):
+        rotated_rect = self.image.get_rect(center=(self.x, self.y))
+        screen.blit(self.image, rotated_rect.topleft)
 
     def move_towards_player(self, player_x, player_y):
         dx = player_x - self.x
@@ -26,6 +29,11 @@ class Enemy:
         if distance > 0:
             self.x += (dx / distance) * self.speed
             self.y += (dy / distance) * self.speed
+
+            angle = -(dx / distance) * 90
+            if dy < 0:
+                angle = 180 - angle
+            self.image = pygame.transform.rotate(self.original_image, angle)
 
     def attack(self, player, damage=1):
         current_time = pygame.time.get_ticks()
